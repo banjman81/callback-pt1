1a2232b4f1ebc5e2b3ed3907ee5e56a431e9f9e5
[banjman81] <[benjamin.man97@gmail.com]>
2021-09-08 13:03:52 -0500

    solved

--- functions.js
+++ functions.js
@@ -1,13 +1,68 @@
 const identity = function (value) {
   return value;
 };
-const first = function (array, n) {};
+const first = function (array, n) {
+  let result = []
+  if(n == undefined){
+    return array[0]
+  }
+  else if( n > array.length){
+    return array
+  }
+  else{
+    for(let i = 0; i< n; i++){
+      result.push(array[i])
+    }
+  }
+  return result
+};
 
-const last = function (array, n) {};
+const last = function (array, n) {
+  let result = []
+  if( n == undefined){
+    return array[array.length-1]
+  }
+  else if(n > array.length){
+    return array
+  }
+  else{
+    let counter = 0
+    for(let i = array.length-1; counter < n; i--){
+      result.unshift(array[i])
+      counter++
+    }
+  }
+  return result
+};
 
-const each = function (collection, callback) {};
-const indexOf = function (array, target) {};
-const map = function (collection, iterator) {};
+const each = function (collection, callback) {
+  if (Array.isArray(collection) === true){
+    for( let i = 0; i < collection.length; i++){
+      callback(collection[i], i , collection);
+    }
+  }
+  else{
+    for( let animal in collection){
+      callback(collection[animal], animal, collection)
+    }
+    // for( let animal in collection){
+    //   callback(animal, animal, collection)
+    // }
+  }
+};
+
+const indexOf = function (array, target) {
+  for(let i = 0; i < array.length; i++){
+    if (array[i] == target){
+      return i
+    }
+  }
+  return -1;
+};
+const map = function (collection, iterator) {
+  const result = collection.map( item => iterator(item))
+  return result
+};
 
 module.exports = {
   identity,
--- test.js
+++ test.js
@@ -14,7 +14,7 @@ describe("identity", function () {
   });
 });
 
-//////////
+////////
 
 describe("first", function () {
   it("should be able to pull out the first element of an array", function () {
@@ -34,118 +34,118 @@ describe("first", function () {
   });
 });
 
-// //////////
+//////////
 
-// describe("last", function () {
-//   it("should pull the last element from an array", function () {
-//     expect(last([1, 2, 3])).to.equal(3);
-//   });
+describe("last", function () {
+  it("should pull the last element from an array", function () {
+    expect(last([1, 2, 3])).to.equal(3);
+  });
 
-//   it("should accept an index argument", function () {
-//     expect(last([1, 2, 3], 2)).to.eql([2, 3]);
-//   });
+  it("should accept an index argument", function () {
+    expect(last([1, 2, 3], 2)).to.eql([2, 3]);
+  });
 
-//   it("should return empty array if zero is passed in as the index", function () {
-//     expect(last([1, 2, 3], 0)).to.eql([]);
-//   });
+  it("should return empty array if zero is passed in as the index", function () {
+    expect(last([1, 2, 3], 0)).to.eql([]);
+  });
 
-//   it("should return all the array's elements if the index argument is larger than the length of the array", function () {
-//     expect(last([1, 2, 3], 5)).to.eql([1, 2, 3]);
-//   });
-// });
+  it("should return all the array's elements if the index argument is larger than the length of the array", function () {
+    expect(last([1, 2, 3], 5)).to.eql([1, 2, 3]);
+  });
+});
 
-// ///////////
+///////////
 
-// describe("each", function () {
-//   it("should iterate over arrays, providing access to the element, index, and array itself", function () {
-//     var animals = ["ant", "bat", "cat"];
-//     var iterationInputs = [];
+describe("each", function () {
+  it("should iterate over arrays, providing access to the element, index, and array itself", function () {
+    var animals = ["ant", "bat", "cat"];
+    var iterationInputs = [];
 
-//     each(animals, function (animal, index, list) {
-//       iterationInputs.push([animal, index, list]);
-//     });
+    each(animals, function (animal, index, list) {
+      iterationInputs.push([animal, index, list]);
+    });
 
-//     expect(iterationInputs).to.eql([
-//       ["ant", 0, animals],
-//       ["bat", 1, animals],
-//       ["cat", 2, animals],
-//     ]);
-//   });
+    expect(iterationInputs).to.eql([
+      ["ant", 0, animals],
+      ["bat", 1, animals],
+      ["cat", 2, animals],
+    ]);
+  });
 
-//   it("should only iterate over the array elements, not properties of the array", function () {
-//     var animals = ["ant", "bat", "cat"];
-//     var iterationInputs = [];
+  it("should only iterate over the array elements, not properties of the array", function () {
+    var animals = ["ant", "bat", "cat"];
+    var iterationInputs = [];
 
-//     animals.shouldBeIgnored = "Ignore me!";
+    animals.shouldBeIgnored = "Ignore me!";
 
-//     each(animals, function (animal, index, list) {
-//       iterationInputs.push([animal, index, list]);
-//     });
+    each(animals, function (animal, index, list) {
+      iterationInputs.push([animal, index, list]);
+    });
 
-//     expect(iterationInputs).to.eql([
-//       ["ant", 0, animals],
-//       ["bat", 1, animals],
-//       ["cat", 2, animals],
-//     ]);
-//   });
+    expect(iterationInputs).to.eql([
+      ["ant", 0, animals],
+      ["bat", 1, animals],
+      ["cat", 2, animals],
+    ]);
+  });
 
-//   it("should iterate over objects, providing access to the element, index, and object itself", function () {
-//     var animals = { a: "ant", b: "bat", c: "cat" };
-//     var iterationInputs = [];
+  it("should iterate over objects, providing access to the element, index, and object itself", function () {
+    var animals = { a: "ant", b: "bat", c: "cat" };
+    var iterationInputs = [];
 
-//     each(animals, function (animal, key, object) {
-//       iterationInputs.push([animal, key, object]);
-//     });
+    each(animals, function (animal, key, object) {
+      iterationInputs.push([animal, key, object]);
+    });
+    
+    expect(iterationInputs).to.eql([
+      ["ant", "a", animals],
+      ["bat", "b", animals],
+      ["cat", "c", animals],
+    ]);
+  });
+});
 
-//     expect(iterationInputs).to.eql([
-//       ["ant", "a", animals],
-//       ["bat", "b", animals],
-//       ["cat", "c", animals],
-//     ]);
-//   });
-// });
+describe("indexOf", function () {
+  it("should find 40 in the list", function () {
+    var numbers = [10, 20, 30, 40, 50];
 
-// describe("indexOf", function () {
-//   it("should find 40 in the list", function () {
-//     var numbers = [10, 20, 30, 40, 50];
+    expect(indexOf(numbers, 40)).to.equal(3);
+  });
 
-//     expect(indexOf(numbers, 40)).to.equal(3);
-//   });
+  it("should be able to compute indexOf even when the native function is undefined", function () {
+    var numbers = [10, 20, 30];
 
-//   it("should be able to compute indexOf even when the native function is undefined", function () {
-//     var numbers = [10, 20, 30];
+    expect(indexOf(numbers, 20)).to.equal(1);
+  });
 
-//     expect(indexOf(numbers, 20)).to.equal(1);
-//   });
+  it("returns -1 when the target cannot be found not in the list", function () {
+    var numbers = [10, 20, 30, 40, 50];
 
-//   it("returns -1 when the target cannot be found not in the list", function () {
-//     var numbers = [10, 20, 30, 40, 50];
+    expect(indexOf(numbers, 35)).to.equal(-1);
+  });
 
-//     expect(indexOf(numbers, 35)).to.equal(-1);
-//   });
+  it("returns the first index that the target can be found at when there are multiple matches", function () {
+    var numbers = [1, 40, 40, 40, 40, 40, 40, 40, 50, 60, 70];
 
-//   it("returns the first index that the target can be found at when there are multiple matches", function () {
-//     var numbers = [1, 40, 40, 40, 40, 40, 40, 40, 50, 60, 70];
+    expect(indexOf(numbers, 40)).to.equal(1);
+  });
+});
 
-//     expect(indexOf(numbers, 40)).to.equal(1);
-//   });
-// });
+describe("map", function () {
+  it("should apply a function to every value in an array", function () {
+    var doubledNumbers = map([1, 2, 3], function (num) {
+      return num * 2;
+    });
 
-// describe("map", function () {
-//   it("should apply a function to every value in an array", function () {
-//     var doubledNumbers = map([1, 2, 3], function (num) {
-//       return num * 2;
-//     });
+    expect(doubledNumbers).to.eql([2, 4, 6]);
+  });
 
-//     expect(doubledNumbers).to.eql([2, 4, 6]);
-//   });
+  it("should produce a brand new array instead of modifying the input array", function () {
+    var numbers = [1, 2, 3];
+    var mappedNumbers = map(numbers, function (num) {
+      return num;
+    });
 
-//   it("should produce a brand new array instead of modifying the input array", function () {
-//     var numbers = [1, 2, 3];
-//     var mappedNumbers = map(numbers, function (num) {
-//       return num;
-//     });
-
-//     expect(mappedNumbers).to.not.equal(numbers);
-//   });
-// });
+    expect(mappedNumbers).to.not.equal(numbers);
+  });
+});
